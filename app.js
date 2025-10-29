ec778426f85927bae8d5ac239840fc5805ae5099
Hilarymb <162254186+Hilarymb@users.noreply.github.com>
2025-10-29 15:15:48 -0500

    Updated app.js and .env with database connection

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+// Import dependencies
 const express = require('express');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const app = express();
@@ -6,6 +8,23 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON
 app.use(express.json());
+
+// Create MySQL connection
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS, //from .env
+  database: process.env.DB_NAME,
+});
+// Connect to MySQL
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed:', err);
+  }
+  else {
+    console.log('Connected to MySQL database');
+  }
+});
 
 // Test route
 app.get('/', (req, res) => {
